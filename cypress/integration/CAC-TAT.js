67ad22761235c97a3c45b37653f34570123e8532
caroline Bauer <bauercarol@hotmail.com>
2022-11-03 19:43:45 -0300

    digitar em campos e clicar em elementos

--- cypress/integration/CAC-TAT.js
+++ cypress/integration/CAC-TAT.js
@@ -8,9 +8,21 @@
 ///referencer types="cypress"/>
 
 describe ('Central de Atendimento ao cliente TAT', function(){
-    it ('verifica o título de aplicação', function(){
+    beforeEach(function(){
         cy.visit('./src/index.html')
-
+    })
+    it ('verifica o título de aplicação', function(){
         cy.title().should('be.equal','Central de Atendimento ao Cliente TAT')
+    })
+
+    it.only ('preenche os campos obrigatórios e envia o formulário', function(){
+        cy.get('#firstName').type('Caroline')
+        cy.get('#lastName').type('Bauer')
+        cy.get('#email').type('bauercarol@hotmail.com')
+        cy.get('#open-text-area').type('teste')
+        cy.get('button[type="submit"]').click()
+
+        cy.get('.success > strong').should('be.visible')
+
     })
 })
