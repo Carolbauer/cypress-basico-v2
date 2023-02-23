cc94c4e4996813517167ddb34cc485413f6ef2ff
carolbauer <caroline.bauer@ubistart.com>
2023-02-23 14:29:11 -0300

    invoke

--- cypress/integration/CAC-TAT.js
+++ cypress/integration/CAC-TAT.js
@@ -171,6 +171,32 @@ describe ('Central de Atendimento ao cliente TAT', function(){
         cy.contains('Talking About Testing').should('be.visible')
         
     })
+
+    it('exibe e esconde as mensagens de sucesso e erro usando o .invoke', () => {
+        cy.get('.success')
+          .should('not.be.visible')
+          .invoke('show')
+          .should('be.visible')
+          .and('contain', 'Mensagem enviada com sucesso.')
+          .invoke('hide')
+          .should('not.be.visible')
+        cy.get('.error')
+          .should('not.be.visible')
+          .invoke('show')
+          .should('be.visible')
+          .and('contain', 'Valide os campos obrigatórios!')
+          .invoke('hide')
+          .should('not.be.visible')
+      })
+
+    it.only('preeenche a area de texto usando o comando invoke', function(){
+        const longText = Cypress._.repeat('0123456789', 20)
+
+        cy.get('#open-text-area')
+    })
+
+
+    
           
          
         
